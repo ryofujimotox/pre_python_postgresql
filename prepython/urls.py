8b3147bfb5043aa7846a9328d67c574bef2acde0
藤本凌 <fujimotoryou@fujimotoryous-MacBook-Air.local>
2023-09-03 17:45:23 +0900

    💡test追加

--- prepython/urls.py
+++ prepython/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from . import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test.test, name='test'),
 ]
